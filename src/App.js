768b744bbb68183a375c8e1a89dd4606270d3a9b
Michael Jarvis <mjarvis9541@gmail.com>
2021-02-02 16:57:53 +0000

    Added reminder true/false on task double click

--- src/App.js
+++ src/App.js
@@ -26,14 +26,28 @@ function App() {
 
   // Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
     // console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    // console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to show.'}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show."
+      )}
     </div>
   );
 }
